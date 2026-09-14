e1a2e97d164ec7991bd248087e92842a152c123e
redriz <rodrigz.dzn@gmail.com>
2026-09-14 11:16:01 +0100

    Implement user registration logic: validate input fields and create user account

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
 class AuthController extends BaseController
 {
     public function loginForm()
@@ -21,5 +23,28 @@ class AuthController extends BaseController
         $email = $_POST['email'];
         $password = $_POST['password'];
         $passwordConfirmation = $_POST['password_confirmation'];
+
+        if (empty($name) || empty($username) || empty($email) || empty($password) || empty($passwordConfirmation)) {
+            $this->render('auth/register', ['title' => 'Criar conta', 'error' => 'Deve preencher todos os campos']);
+            return;
+        }
+
+        if ($password !== $passwordConfirmation) {
+            $this->render('auth/register', ['title' => 'Criar conta', 'error' => 'As palavras-passes não coincidem']);
+            return;
+        }
+
+        if (strlen($password) < 8) {
+            $this->render('auth/register', ['title' => 'Criar conta', 'error' => 'Palavra-passe deve ter no mínimo 8 caracteres']);
+            return;
+        }
+
+        if (User::emailOrUsernameExists($email, $username)) {
+            $this->render('auth/register', ['title' => 'Criar conta', 'error' => 'Username ou Email já está em uso']);
+            return;
+        }
+
+        $id = User::create($name, $username, $email, $password);
+        echo "Usuário crido com sucesso! ID: " . $id;
     }
 }
